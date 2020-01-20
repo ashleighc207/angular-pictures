6845f451c902390dc2f37bf4929a16c51c00d5a0
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-20 10:45:43 -0500

    get image per page working

--- src/app/picture/picture.component.ts
+++ src/app/picture/picture.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Input } from "@angular/core";
 export class PictureComponent implements OnInit {
   @Input() title = "";
   @Input() url = "";
-
   constructor() {}
 
   ngOnInit() {}
